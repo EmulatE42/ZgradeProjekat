ebc0bb2e55ec3173f3e19b29d794dc88bf89d006
Momir <mkosticfcb95@gmail.com>
2018-01-17 20:30:06 +0100

    add voting option

--- src/main/java/com/ftn/ZgradeProjekat/domain/Topic.java
+++ src/main/java/com/ftn/ZgradeProjekat/domain/Topic.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by djuro on 11/4/2017.
@@ -35,19 +37,27 @@ public class Topic
     @Column(name = "topic_accepted")
     private Boolean accepted;
 
-    @Column(name = "topic_votes")
-    private Long votes;
+    @Column(name = "pos_votes")
+    private Long pos_votes;
+
+    @Column(name = "neg_votes")
+    private Long neg_votes;
 
     @ManyToOne(cascade=CascadeType.MERGE)
     @JsonIgnore
     private Session session;
 
-    public Topic(TopicDTO topicDTO)
+    @OneToMany(mappedBy = "topic", fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+    private Set<Vote> votes = new HashSet<Vote>();
+
+    public Topic(TopicDTO topicDTO, Session session)
     {
         this.id = topicDTO.getId();
         this.description = topicDTO.getDescription();
         this.creator = topicDTO.getCreator();
         this.accepted = topicDTO.getAccepted();
-        this.votes = topicDTO.getVotes();
+        this.pos_votes = topicDTO.getPos_votes();
+        this.neg_votes = topicDTO.getNeg_votes();
+        this.session = session;
     }
 }
